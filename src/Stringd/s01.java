047aa9e5d02a866c946fcde960a1c61ad462396e
Naveen <naveen.rp.2805@gmail.com>
2025-10-28 10:36:36 +0530

    Commit 23

---
+++ src/Stringd/s01.java
@@ -0,0 +1,37 @@
+package Stringd;
+
+public class s01 {
+
+    public static void main(String[] args) {
+        String s = "program";
+        String s1 = new String("java");
+        String s2 = "java";
+        String s3 = "program";
+        s.concat("ing");
+        s1 = s1.concat("Programming");
+        String s4 = "Program";
+        String s5 = new String("Program");
+        System.out.println(s);
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        System.out.println(s==s1);
+        System.out.println(s==s2);
+        System.out.println(s==s3);
+        System.out.println(s==s4);
+        System.out.println(s5==s1);
+        System.out.println(s5==s4); //s5 pointing to heap || s4 is pointing to scp area.
+
+//pending:
+//        System.out.println(s.equals(s1));
+//        System.out.println(s.equals(s2));
+//        System.out.println(s.equals(s3));
+//        System.out.println(s.equals(s4));
+
+
+
+
+    }
+}
